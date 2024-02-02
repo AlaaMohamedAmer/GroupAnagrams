5adf708c8d09687fd29212f4c274a9551e2e5808
alaa <aamal79545@gmail.com>
2024-02-02 20:08:30 +0200

    first commit

---
+++ GroupAnagrams/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupAnagrams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s1 = new Solution();
+
+            IList<IList<string>> result = s1.GroupAnagrams(new string[] { "eat", "tea", "tan", "ate", "nat", "bat" });
+
+           foreach (var group in result)
+           {
+               Console.WriteLine(string.Join(", ", group));
+           }
+            Console.ReadKey();
+        }
+    }
+    public class Solution
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            var dict = new Dictionary<string, IList<string>>();
+            for (int i = 0; i < strs.Length; i++)
+            {
+                char[] arr= strs[i].ToCharArray();
+                Array.Sort(arr);
+                string sorted = new string(arr);
+                if (!dict.ContainsKey(sorted))
+                {
+                    dict.Add(sorted, new List<string>() { strs[i] })  ;   
+                }
+                else
+                {
+                    dict[sorted].Add(strs[i]) ;
+                }
+
+            }
+            return dict.Values.ToList();
+        }
+    }
+}
